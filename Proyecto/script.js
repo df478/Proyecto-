c95e34044cdf8bb6cb716be915aaaef3c0ecb7ce
df478 <86130705+df478@users.noreply.github.com>
2021-11-29 10:58:49 -0400

    Add files via upload

---
+++ Proyecto/script.js
@@ -0,0 +1,335 @@
+var b=document.getElementById("boton1");
+b.addEventListener("click",n1);
+function n1(){
+  var salida1="";
+  salida1=salida1+"Croquetas para perro."
+
+  document.getElementById("n1").innerHTML=salida1
+}
+var b=document.getElementById("boton1");
+b.addEventListener("click",c1);
+var cantidad1=0;
+function c1(){
+  cantidad1+=1;
+  document.getElementById("c1").innerHTML=cantidad1
+}
+var b=document.getElementById("boton1");
+b.addEventListener("click",p1);
+var precio1=0;
+function p1(){ 
+  precio1+=5
+  document.getElementById("p1").innerHTML=precio1
+}
+var b=document.getElementById("boton1-r");
+b.addEventListener("click",r1);
+function r1(){
+  cantidad1-=1;
+  if(cantidad1<0){
+    cantidad1=0
+  }
+  document.getElementById("c1").innerHTML=cantidad1
+}
+var b=document.getElementById("boton1-r");
+b.addEventListener("click",rp1);
+function rp1(){
+  precio1-=5;
+  if(precio1<0){
+    precio1=0
+  }
+  document.getElementById("p1").innerHTML=precio1
+}
+//Segundo producto
+var b=document.getElementById("boton2");
+b.addEventListener("click",n2);
+function n2(){
+  var salida2="";
+  salida2=salida2+"Collares para perro."
+
+  document.getElementById("n2").innerHTML=salida2
+}
+var b=document.getElementById("boton2");
+b.addEventListener("click",c2);
+var cantidad2=0;
+function c2(){
+  cantidad2+=1;
+  document.getElementById("c2").innerHTML=cantidad2
+}
+var b=document.getElementById("boton2");
+b.addEventListener("click",p2);
+var precio2=0;
+function p2(){ 
+  precio2+=5
+  document.getElementById("p2").innerHTML=precio2
+}
+var b=document.getElementById("boton2-r");
+b.addEventListener("click",r2);
+function r2(){
+  cantidad2-=1;
+  if(cantidad2<0){
+    cantidad2=0
+  }
+  document.getElementById("c2").innerHTML=cantidad2
+}
+var b=document.getElementById("boton2-r");
+b.addEventListener("click",rp2);
+function rp2(){
+  precio2-=5;
+  if(precio2<0){
+    precio2=0
+  }
+  document.getElementById("p2").innerHTML=precio2
+}
+//tercer producto
+var b=document.getElementById("boton3");
+b.addEventListener("click",n3);
+function n3(){
+  var salida3="";
+  salida3=salida3+"Corta Uñas para mascotas."
+
+  document.getElementById("n3").innerHTML=salida3
+}
+var b=document.getElementById("boton3");
+b.addEventListener("click",c3);
+var cantidad3=0;
+function c3(){
+  cantidad3+=1;
+  document.getElementById("c3").innerHTML=cantidad3
+}
+var b=document.getElementById("boton3");
+b.addEventListener("click",p3);
+var precio3=0;
+function p3(){ 
+  precio3+=5
+  document.getElementById("p3").innerHTML=precio3
+}
+var b=document.getElementById("boton3-r");
+b.addEventListener("click",r3);
+function r3(){
+  cantidad3-=1;
+  if(cantidad3<0){
+    cantidad3=0
+  }
+  document.getElementById("c3").innerHTML=cantidad3
+}
+var b=document.getElementById("boton3-r");
+b.addEventListener("click",rp3);
+function rp3(){
+  precio3-=5;
+  if(precio3<0){
+    precio3=0
+  }
+  document.getElementById("p3").innerHTML=precio3
+}
+//cuarto producto
+var b=document.getElementById("boton4");
+b.addEventListener("click",n4);
+function n4(){
+  var salida4="";
+  salida4=salida4+"Tazón de comida."
+
+  document.getElementById("n4").innerHTML=salida4
+}
+var b=document.getElementById("boton4");
+b.addEventListener("click",c4);
+var cantidad4=0;
+function c4(){
+  cantidad4+=1;
+  document.getElementById("c4").innerHTML=cantidad4
+}
+var b=document.getElementById("boton4");
+b.addEventListener("click",p4);
+var precio4=0;
+function p4(){ 
+  precio4+=5
+  document.getElementById("p4").innerHTML=precio4
+}
+var b=document.getElementById("boton4-r");
+b.addEventListener("click",r4);
+function r4(){
+  cantidad4-=1;
+  if(cantidad4<0){
+    cantidad4=0
+  }
+  document.getElementById("c4").innerHTML=cantidad4
+}
+var b=document.getElementById("boton4-r");
+b.addEventListener("click",rp4);
+function rp4(){
+  precio4-=5;
+  if(precio4<0){
+    precio4=0
+  }
+  document.getElementById("p4").innerHTML=precio4
+}
+//quinto producto
+var b=document.getElementById("boton5");
+b.addEventListener("click",n5);
+function n5(){
+  var salida5="";
+  salida5=salida5+"Juguete para perro."
+
+  document.getElementById("n5").innerHTML=salida5
+}
+var b=document.getElementById("boton5");
+b.addEventListener("click",c5);
+var cantidad5=0;
+function c5(){
+  cantidad5+=1;
+  document.getElementById("c5").innerHTML=cantidad5
+}
+var b=document.getElementById("boton5");
+b.addEventListener("click",p5);
+var precio5=0;
+function p5(){ 
+  precio5+=5
+  document.getElementById("p5").innerHTML=precio5
+}
+var b=document.getElementById("boton5-r");
+b.addEventListener("click",r5);
+function r5(){
+  cantidad5-=1;
+  if(cantidad5<0){
+    cantidad5=0
+  }
+  document.getElementById("c5").innerHTML=cantidad5
+}
+var b=document.getElementById("boton5-r");
+b.addEventListener("click",rp5);
+function rp5(){
+  precio5-=5;
+  if(precio5<0){
+    precio5=0
+  }
+  document.getElementById("p5").innerHTML=precio5
+}
+//sexto producto
+var b=document.getElementById("boton6");
+b.addEventListener("click",n6);
+function n6(){
+  var salida6="";
+  salida6=salida6+"Shampoo para perro."
+
+  document.getElementById("n6").innerHTML=salida6
+}
+var b=document.getElementById("boton6");
+b.addEventListener("click",c6);
+var cantidad6=0;
+function c6(){
+  cantidad6+=1;
+  document.getElementById("c6").innerHTML=cantidad6
+}
+var b=document.getElementById("boton6");
+b.addEventListener("click",p6);
+var precio6=0;
+function p6(){ 
+  precio6+=5
+  document.getElementById("p6").innerHTML=precio6
+}
+var b=document.getElementById("boton6-r");
+b.addEventListener("click",r6);
+function r6(){
+  cantidad6-=1;
+  if(cantidad6<0){
+    cantidad6=0
+  }
+  document.getElementById("c6").innerHTML=cantidad6
+}
+var b=document.getElementById("boton6-r");
+b.addEventListener("click",rp6);
+function rp6(){
+  precio6-=5;
+  if(precio6<0){
+    precio6=0
+  }
+  document.getElementById("p6").innerHTML=precio6
+}
+//septimo producto
+var b=document.getElementById("boton7");
+b.addEventListener("click",n7);
+function n7(){
+  var salida7="";
+  salida7=salida7+"Chaleco para perro."
+
+  document.getElementById("n7").innerHTML=salida7
+}
+var b=document.getElementById("boton7");
+b.addEventListener("click",c7);
+var cantidad7=0;
+function c7(){
+  cantidad7+=1;
+  document.getElementById("c7").innerHTML=cantidad7
+}
+var b=document.getElementById("boton7");
+b.addEventListener("click",p7);
+var precio7=0;
+function p7(){ 
+  precio7+=5
+  document.getElementById("p7").innerHTML=precio7
+}
+var b=document.getElementById("boton7-r");
+b.addEventListener("click",r7);
+function r7(){
+  cantidad7-=1;
+  if(cantidad7<0){
+    cantidad7=0
+  }
+  document.getElementById("c7").innerHTML=cantidad7
+}
+var b=document.getElementById("boton7-r");
+b.addEventListener("click",rp7);
+function rp7(){
+  precio7-=5;
+  if(precio7<0){
+    precio7=0
+  }
+  document.getElementById("p7").innerHTML=precio7
+}
+//octavo producto
+var b=document.getElementById("boton8");
+b.addEventListener("click",n8);
+function n8(){
+  var salida8="";
+  salida8=salida8+"Cepillo de dientes."
+
+  document.getElementById("n8").innerHTML=salida8
+}
+var b=document.getElementById("boton8");
+b.addEventListener("click",c8);
+var cantidad8=0;
+function c8(){
+  cantidad8+=1;
+  document.getElementById("c8").innerHTML=cantidad8
+}
+var b=document.getElementById("boton8");
+b.addEventListener("click",p8);
+var precio8=0;
+function p8(){ 
+  precio8+=5
+  document.getElementById("p8").innerHTML=precio8
+}
+var b=document.getElementById("boton8-r");
+b.addEventListener("click",r8);
+function r8(){
+  cantidad8-=1;
+  if(cantidad8<0){
+    cantidad8=0
+  }
+  document.getElementById("c8").innerHTML=cantidad8
+}
+var b=document.getElementById("boton8-r");
+b.addEventListener("click",rp8);
+function rp8(){
+  precio8-=5;
+  if(precio8<0){
+    precio8=0
+  }
+  document.getElementById("p8").innerHTML=precio8
+}
+//precio total
+var precio=0;
+var b=document.getElementById("CalcularTotal");
+b.addEventListener("click",preciototal);
+function preciototal(){
+  precio=precio1+precio2+precio3+precio4+precio5+precio6+precio7+precio8
+  document.getElementById("total").innerHTML=precio
+}
